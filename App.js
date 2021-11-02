8d1e81e0650590e1c9a73dfed5b881570abe3510
Mohammed Imran <mohdimran97057@gmail.com>
2021-11-02 19:03:56 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Header from './components/Layout/Header';
+import React, { Fragment } from 'react';
+
+function App() {
+  return (
+    <Fragment>
+      <Header></Header>
+    </Fragment>
+  );
+}
+
+export default App;
