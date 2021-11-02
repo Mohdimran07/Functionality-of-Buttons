2de1a41720a14837aaa53aff8256db632ffb08f4
Mohammed Imran <mohdimran97057@gmail.com>
2021-11-02 19:47:56 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 
-import './App.css';
+
 import Header from './components/Layout/Header';
 import React, { Fragment } from 'react';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header></Header>
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
