d1b93e9ad6656aca8057fc874ecf769692d0528f
Mohammed Imran <mohdimran97057@gmail.com>
2021-11-03 11:17:46 +0530

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import Cart from './components/Cart/Cart';
 
 import Header from './components/Layout/Header';
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Meals from './components/Meals/Meals';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+  
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
   return (
     <Fragment>
-      <Cart />
-      <Header></Header>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}></Header>
       <main>
         <Meals />
       </main>
